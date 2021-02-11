c4a4a8e1b9503912400fc433eaa99d72260043a8
xps <kandeldeepak46@gmail.com>
2021-02-11 09:51:51 +0545

    code refactored and reformatted with black

--- sentiment_analyzer/classifier/train_model.py
+++ sentiment_analyzer/classifier/train_model.py
@@ -1,4 +1,3 @@
-
 from collections import defaultdict
 from textwrap import wrap
 
@@ -46,7 +45,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 PRE_TRAINED_MODEL_NAME = "bert-base-cased"
-class_names = ['negative', 'neutral', 'positive']
+class_names = ["negative", "neutral", "positive"]
 
 
 class GPReviewDataset(Dataset):
@@ -91,20 +90,20 @@ def to_sentiment(rating):
     else:
         return 2
 
-df = pd.read_csv('./notebooks/reviews.csv')
-df['sentiment'] = df.score.apply(to_sentiment)
+
+df = pd.read_csv("./notebooks/reviews.csv")
+df["sentiment"] = df.score.apply(to_sentiment)
 tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
 
 
 class GPReviewDataset(Dataset):
-
     def __init__(self, reviews, targets, tokenizer, max_len):
 
         self.reviews = reviews
         self.targets = targets
         self.tokenizer = tokenizer
         self.max_len = max_len
-    
+
     def __len__(self):
 
         return len(self.reviews)
@@ -119,67 +118,50 @@ class GPReviewDataset(Dataset):
             return_token_type_ids=False,
             pad_to_max_length=True,
             return_attention_mask=True,
-            return_tensors='pt',
-            )
+            return_tensors="pt",
+        )
 
         return {
-            'review_text': review,
-            'input_ids': encoding['input_ids'].flatten(),
-            'attention_mask': encoding['attention_mask'].flatten(),
-            'targets': torch.tensor(target, dtype=torch.long)
-            }
+            "review_text": review,
+            "input_ids": encoding["input_ids"].flatten(),
+            "attention_mask": encoding["attention_mask"].flatten(),
+            "targets": torch.tensor(target, dtype=torch.long),
+        }
 
 
-df_train, df_test = train_test_split(
-  df,
-  test_size=0.1,
-  random_state=RANDOM_SEED
-)
-df_val, df_test = train_test_split(
-  df_test,
-  test_size=0.5,
-  random_state=RANDOM_SEED
-)
+df_train, df_test = train_test_split(df, test_size=0.1, random_state=RANDOM_SEED)
+df_val, df_test = train_test_split(df_test, test_size=0.5, random_state=RANDOM_SEED)
 
 
 def create_data_loader(df, tokenizer, max_len, batch_size):
     ds = GPReviewDataset(
-    reviews=df.content.to_numpy(),
-    targets=df.sentiment.to_numpy(),
-    tokenizer=tokenizer,
-    max_len=max_len
-  )
-    return DataLoader(
+        reviews=df.content.to_numpy(),
+        targets=df.sentiment.to_numpy(),
+        tokenizer=tokenizer,
+        max_len=max_len,
+    )
 
-
-       ds,
-       batch_size=batch_size,
-       num_workers=4
-  )
+    return DataLoader(ds, batch_size=batch_size, num_workers=4)
 
 
 train_data_loader = create_data_loader(df_train, tokenizer, MAX_LEN, BATCH_SIZE)
 val_data_loader = create_data_loader(df_val, tokenizer, MAX_LEN, BATCH_SIZE)
 test_data_loader = create_data_loader(df_test, tokenizer, MAX_LEN, BATCH_SIZE)
 
+
 class SentimentClassifier(nn.Module):
     def __init__(self, n_classes):
-
 
         super(SentimentClassifier, self).__init__()
         self.bert = BertModel.from_pretrained(PRE_TRAINED_MODEL_NAME)
         self.drop = nn.Dropout(p=0.3)
         self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
+
     def forward(self, input_ids, attention_mask):
 
-        _, pooled_output = self.bert(
-
-            input_ids=input_ids,
-            attention_mask=attention_mask
-        )
+        _, pooled_output = self.bert(input_ids=input_ids, attention_mask=attention_mask)
         output = self.drop(pooled_output)
         return self.out(output)
-
 
 
 model = SentimentClassifier(len(class_names))
@@ -189,14 +171,12 @@ EPOCHS = 10
 optimizer = AdamW(model.parameters(), lr=2e-5, correct_bias=False)
 total_steps = len(train_data_loader) * EPOCHS
 scheduler = get_linear_schedule_with_warmup(
-    optimizer,
-    num_warmup_steps=0,
-    num_training_steps=total_steps
-    )
+    optimizer, num_warmup_steps=0, num_training_steps=total_steps
+)
 loss_fn = nn.CrossEntropyLoss().to(device)
 
 
-def train_epoch(model,data_loader,loss_fn,optimizer,device,scheduler,n_examples):
+def train_epoch(model, data_loader, loss_fn, optimizer, device, scheduler, n_examples):
     model = model.train()
     losses = []
     correct_predictions = 0
@@ -204,10 +184,7 @@ def train_epoch(model,data_loader,loss_fn,optimizer,device,scheduler,n_examples)
         input_ids = d["input_ids"].to(device)
         attention_mask = d["attention_mask"].to(device)
         targets = d["targets"].to(device)
-        outputs = model(
-            input_ids=input_ids,
-            attention_mask=attention_mask
-            )
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
         _, preds = torch.max(outputs, dim=1)
         loss = loss_fn(outputs, targets)
         correct_predictions += torch.sum(preds == targets)
@@ -219,6 +196,7 @@ def train_epoch(model,data_loader,loss_fn,optimizer,device,scheduler,n_examples)
         optimizer.zero_grad()
     return correct_predictions.double() / n_examples, np.mean(losses)
 
+
 def eval_model(model, data_loader, loss_fn, device, n_examples):
     model = model.eval()
     losses = []
@@ -229,10 +207,7 @@ def eval_model(model, data_loader, loss_fn, device, n_examples):
             input_ids = d["input_ids"].to(device)
             attention_mask = d["attention_mask"].to(device)
             targets = d["targets"].to(device)
-            outputs = model(
-                input_ids=input_ids,
-                attention_mask=attention_mask
-            )
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
             _, preds = torch.max(outputs, dim=1)
             loss = loss_fn(outputs, targets)
             correct_predictions += torch.sum(preds == targets)
@@ -240,13 +215,13 @@ def eval_model(model, data_loader, loss_fn, device, n_examples):
         return correct_predictions.double() / n_examples, np.mean(losses)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     history = defaultdict(list)
     best_accuracy = 0
     for epoch in range(EPOCHS):
-        print(f'Epoch {epoch + 1}/{EPOCHS}')
-        print('-' * 10)
+        print(f"Epoch {epoch + 1}/{EPOCHS}")
+        print("-" * 10)
         train_acc, train_loss = train_epoch(
             model,
             train_data_loader,
@@ -254,24 +229,20 @@ if __name__ == '__main__':
             optimizer,
             device,
             scheduler,
-            len(df_train)
-            )
-        print(f'Train loss {train_loss} accuracy {train_acc}')
+            len(df_train),
+        )
+        print(f"Train loss {train_loss} accuracy {train_acc}")
         val_acc, val_loss = eval_model(
+            model, val_data_loader, loss_fn, device, len(df_val)
+        )
 
-            model,
-            val_data_loader,
-            loss_fn,
-            device,
-            len(df_val)
-            )
-        print(f'Val   loss {val_loss} accuracy {val_acc}')
+        print(f"Val   loss {val_loss} accuracy {val_acc}")
         print()
-        history['train_acc'].append(train_acc)
-        history['train_loss'].append(train_loss)
-        history['val_acc'].append(val_acc)
-        history['val_loss'].append(val_loss)
+        history["train_acc"].append(train_acc)
+        history["train_loss"].append(train_loss)
+        history["val_acc"].append(val_acc)
+        history["val_loss"].append(val_loss)
         if val_acc > best_accuracy:
 
-            torch.save(model.state_dict(), 'best_model_state.bin')
+            torch.save(model.state_dict(), "best_model_state.bin")
             best_accuracy = val_acc
