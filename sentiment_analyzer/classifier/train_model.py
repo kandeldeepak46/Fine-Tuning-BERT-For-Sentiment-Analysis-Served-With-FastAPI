1e0b4fdf22f696c3a9882f00553c71dbef9dd64f
xps <kandeldeepak46@gmail.com>
2021-02-14 16:09:53 +0545

    code refactoring and minor changes in logic

--- sentiment_analyzer/classifier/train_model.py
+++ sentiment_analyzer/classifier/train_model.py
@@ -82,7 +82,9 @@ class GPReviewDataset(Dataset):
 
 
 def to_sentiment(rating):
-
+    """
+    converting fine grained sentiments to 3 levels of sentiment
+    """
     rating = int(rating)
     if rating <= 2:
         return 0
@@ -186,21 +188,25 @@ def train_epoch(
     model = model.train()
     losses = []
     correct_predictions = 0
-    for d in data_loader:
-        input_ids = d["input_ids"].to(device)
-        attention_mask = d["attention_mask"].to(device)
-        targets = d["targets"].to(device)
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-        _, preds = torch.max(outputs, dim=1)
-        loss = loss_fn(outputs, targets)
-        correct_predictions += torch.sum(preds == targets)
-        losses.append(loss.item())
-        loss.backward()
-        nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        optimizer.step()
-        scheduler.step()
-        optimizer.zero_grad()
-    return correct_predictions.double() / n_examples, np.mean(losses)
+    try:
+
+        for d in data_loader:
+            input_ids = d["input_ids"].to(device)
+            attention_mask = d["attention_mask"].to(device)
+            targets = d["targets"].to(device)
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            _, preds = torch.max(outputs, dim=1)
+            loss = loss_fn(outputs, targets)
+            correct_predictions += torch.sum(preds == targets)
+            losses.append(loss.item())
+            loss.backward()
+            nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            optimizer.step()
+            scheduler.step()
+            optimizer.zero_grad()
+        return correct_predictions.double() / n_examples, np.mean(losses)
+    except Exception as e:
+        raise ValueError("could not train the module")
 
 
 def eval_model(model, data_loader, loss_fn, device, n_examples) -> list:
